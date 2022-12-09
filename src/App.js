25781f77f7c69bc0f3c7736bf6c8e93954d49d24
Yulia Gutsal <julik.ua@gmail.com>
2022-12-09 09:15:22 +0200

    add function WelcomeNew

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const MyComponent = () =>{
+  return <div> Yulia</div>
+}
+
+function Welcome(props){
+  return <h1>Hello! {props.children}</h1>
+}
+
+function Question(props){
+  return <div>{props.children} Do you like React?</div>
+}
+
+function WelcomeNew(props){
+  return(
+    <div>
+      {props.children ? props.children : "Yes, I would like to know it better!"}
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +29,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Welcome>
+          <MyComponent/>
+          <Question/>
+        </Welcome>
+
+        <WelcomeNew/>
         <a
           className="App-link"
           href="https://reactjs.org"
